f670779712ed386249f34937e3dacbf9ef78351d
Jessica Basa <jbasa@stripe.com>
2025-10-03 23:22:50 +0100

    Add FinancialAccountDiclosure component demo

---
+++ examples/hooks/13-FA-Disclosure.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {loadStripe} from '@stripe/stripe-js';
+import FinancialAccountDisclosure from '../../src/components/FinancialAccountDisclosure';
+
+import '../styles/common.css';
+
+const onLoad = () => {
+  console.log('Disclosure has loaded');
+};
+
+const onError = (error) => {
+  console.log('Disclosure error:', error);
+};
+
+const App = () => {
+  const stripe = loadStripe('pk_test_xxx');
+  
+  return (
+    <>
+      <FinancialAccountDisclosure
+        stripe={stripe}
+        onLoad={onLoad}
+        onError={onError}
+        options={{
+          businessName: 'Your Business Name',
+          learnMoreLink: 'https://docs.stripe.com/treasury',
+        }}
+      />
+    </>
+  );
+};
+
+export default App;
