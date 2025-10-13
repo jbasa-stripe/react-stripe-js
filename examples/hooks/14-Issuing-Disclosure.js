c0510f0103383f0716e6d584198ddddb9218e5cc
Jessica Basa <jbasa@stripe.com>
2025-10-13 18:47:49 +0100

    Add IssuingDisclosure component demo

---
+++ examples/hooks/14-Issuing-Disclosure.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {loadStripe} from '@stripe/stripe-js';
+import IssuingDisclosure from '../../src/components/IssuingDisclosure';
+
+import '../styles/common.css';
+
+const onLoad = () => {
+  console.log('Disclosure has loaded');
+};
+
+const onError = (error) => {
+  console.log('Disclosure error:', error);
+};
+
+const App = () => {
+  const stripe = loadStripe('pk_test_xxx');
+  
+  return (
+    <>
+      <IssuingDisclosure
+        stripe={stripe}
+        onLoad={onLoad}
+        onError={onError}
+        options={{
+          publicCardProgramName: 'The Greatest Card Program',
+          learnMoreLink: 'https://docs.stripe.com/issuing',
+        }}
+      />
+    </>
+  );
+};
+
+export default App;
